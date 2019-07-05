088c11a7bd1105aca98f2fb8333606619ca24a89
gaogaostegosaurus <gaogaostegosaurus@users.noreply.github.com>
2019-07-05 02:25:11 -0400

    Kenki tweaks

--- next/sam.js
+++ next/sam.js
@@ -373,7 +373,7 @@ function samKenki() {
 
   // Set Kenki target
   if (player.level >= 70
-  && checkCooldown("ikishoten", player.ID) > checkCooldown("guren", player.ID
+  && checkCooldown("ikishoten", player.ID) > checkCooldown("guren", player.ID)
   && checkCooldown("guren", player.ID) < 30000)) {
     gauge.target = 70;
   }
