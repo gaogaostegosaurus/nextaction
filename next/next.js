9de6e9b7ad61e1d412cc9438c39b547f85d60ecf
gaogaostegosaurus <gaogaostegosaurus@users.noreply.github.com>
2019-07-13 09:50:30 -0500

    Whoops

--- next/next.js
+++ next/next.js
@@ -56,7 +56,7 @@ var cancelGroups = {};
 document.addEventListener("onPlayerChangedEvent", function(e) {
 
   player = e.detail;
-  player.ID = e.detail.nextid.toString(16).toUpperCase(); // player.nextid.toString(16) is lowercase; using "ID" to designate uppercase lettering
+  player.ID = e.detail.id.toString(16).toUpperCase(); // player.id.toString(16) is lowercase; using "ID" to designate uppercase lettering
 
   // Detects name/job/level change and clears elements
   if (previous.name != player.name || previous.job != player.job || previous.level != player.level) {
@@ -112,7 +112,7 @@ document.addEventListener("onPlayerChangedEvent", function(e) {
 
 document.addEventListener('onTargetChangedEvent', function(e) {
   target = e.detail;
-  target.ID = e.detail.nextid.toString(16).toUpperCase(); //
+  target.ID = e.detail.id.toString(16).toUpperCase(); //
 
   if (player.job == "WHM") {
     whmTargetChangedEvent();
