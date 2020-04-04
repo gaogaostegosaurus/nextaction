8adc110379daa278db5905eeee7916a01b75be5d
gaogaostegosaurus <gaogaostegosaurus@users.noreply.github.com>
2020-04-03 20:38:50 -0400

    Dunno

--- src/1/ui.js
+++ src/1/ui.js
@@ -2,7 +2,7 @@ const resetNext = () => {
   /* Reset timeout and intervals */
   Object.keys(timeout).forEach((property) => { clearTimeout(timeout[property]); });
   Object.keys(interval).forEach((property) => { clearInterval(interval[property]); });
-  
+
   document.getElementById('icon-a').innerHTML = '';
   document.getElementById('icon-b').innerHTML = '';
   document.getElementById('icon-c').innerHTML = '';
