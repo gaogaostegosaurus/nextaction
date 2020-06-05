830025376724898cb5601d8193e5c9cd8642df65
gaogaostegosaurus <gaogaostegosaurus@users.noreply.github.com>
2020-06-05 06:37:47 -0400

    Comments and cleanup

--- src/1/recast.js
+++ src/1/recast.js
@@ -1,24 +1,24 @@
 
-// Adds a recast time to tracker array
 const addRecast = ({
   name,
   property = name.replace(/[\s'-]/g, '').toLowerCase(),
-  time = recast[property],
-  id = player.id,
+  time = recast[property], /* Assigned to -1 to "reset" recast */
+  id = player.id, /* Typically doesn't need to be assigned, but just in case I get around to writing
+   raid type stuff */
 } = {}) => {
-  // Create if array doesn't exist yet
+  /* Create array if it doesn't exist yet */
   if (!recastTracker[property]) {
     recastTracker[property] = [];
   }
 
-  // Look for matching index
+  /* Look for matching ID */
   const match = recastTracker[property].findIndex((entry) => entry.id === id);
 
   if (match > -1) {
-    // Update array if match found
+    /* Update recast if match found */
     recastTracker[property][match] = { id, time: time + Date.now() };
   } else {
-    // Push new entry into array if no matching entry
+    /* Push recast value if no match found */
     recastTracker[property].push({ id, time: time + Date.now() });
   }
 };
@@ -28,19 +28,20 @@ const checkRecast = ({
   property = name.replace(/[\s'-]/g, '').toLowerCase(),
   id = player.id,
 } = {}) => {
-  // Check if array exists
+  /* Return -1 if array doesn't exist */
   if (!recastTracker[property]) {
     return -1;
   }
 
-  // Find matching index
+  /* Find matching ID in array */
   const match = recastTracker[property].findIndex((entry) => entry.id === id);
 
   if (match > -1) {
-    // Returns matching recast time
+    /* Returns recast time matching ID */
+    /* Returns -1 if the number is less than -1 */
     return Math.max(recastTracker[property][match].time - Date.now(), -1);
   }
 
-  // Return a recast time of -1 if no match
+  /* Return -1 if no match */
   return -1;
 };
