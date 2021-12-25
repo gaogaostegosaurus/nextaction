a80d7706895f47d34de91a92b2e72cc99b5f5caf
gaogaostegosaurus <gaogaostegosaurus@users.noreply.github.com>
2021-12-25 12:26:09 -0500

    Adding pseudocode for SMN

--- src/jobs/smn.js
+++ src/jobs/smn.js
@@ -12,8 +12,6 @@ const smnActionMatch = ({
 } = {}) => {
   const actionType = getActionDataProperty({ name: actionName, property: 'type' });
 
-  // ninjutsu is so annoying
-
   if (actionType === 'Mudra' && checkStatusDuration({ statusName: 'Mudra', statusArray }) <= 0 && checkStatusDuration({ statusName: 'Kassatsu', statusArray }) <= 0) {
     // Only add recast if Mudra and Kassatsu are not activated
     addActionRecast({ actionName: 'Mudra', recast: 20, recastArray });
@@ -71,17 +69,75 @@ return 'Summon }
 const smnLoopGCDAction = ({
   
 }) => {
-  const aetherchargeRecast = checkActionRecast({ actionName: 'Aethercharge', })
-  const summonBahamutRecast = checkActionRecast({ actionName: 'Summon Bahamut', })
-  const summonPhoenixRecast = checkActionRecast({ actionName: 'Summon Phoenix', })
+  const aetherchargeRecast = Math.min(
+    checkActionRecast({ actionName: 'Aethercharge', recastArray }),
+    checkActionRecast({ actionName: 'Summon Bahamut', recastArray }),
+    checkActionRecast({ actionName: 'Summon Phoenix', recastArray }),
+  );
 
-  if (summonBahamutRecast < 1) { return 'Summon Bahamut'; }
-  if (something) { return 'Summon Phoenix'; }
-  
-  if (arcanum) { return 'Summon Garuda';
-return 'Summon 
-  if (summonBahamutRecast < 1) { return 'Summon Bahamut'; }
-  if (summonBahamutRecast < 1) { return 'Summon Bahamut'; }
+  // Summon as soon as recharge is done
+  if (aetherchargeRecast < 1) {
+    if (actionData.some((element) => element.name === 'Summon Bahamut')) { return 'Summon Bahamut'; }
+    if (actionData.some((element) => element.name === 'Summon Phoenix')) { return 'Summon Phoenix'; }
+    return 'Aethercharge';
+  }
+
+  // Priority here is somewhat arbritrary...
+  if (arcanum) {
+    if (actionData.some((element) => element.name === 'Summon Garuda II')) { return 'Summon Garuda II'; }
+    if (actionData.some((element) => element.name === 'Summon Garuda')) { return 'Summon Garuda'; }
+    return 'Summon Emerald';
+  }
+
+  if (arcanum) {
+    if (actionData.some((element) => element.name === 'Summon Titan II')) { return 'Summon Titan II'; }
+    if (actionData.some((element) => element.name === 'Summon Titan')) { return 'Summon Titan'; }
+    return 'Summon Topaz';
+  }
+
+  if (arcanum) {
+    if (actionData.some((element) => element.name === 'Summon Ifrit II')) { return 'Summon Ifrit II'; }
+    if (actionData.some((element) => element.name === 'Summon Ifrit')) { return 'Summon Ifrit'; }
+    return 'Summon Ruby';
+  }
+
+  if (attunement === 'Ifrit') {
+    if (checkStatusDuration({ name: 'Ifrit\'s Favor', statusArray }) > 0) {
+      if (comboAction === 'Crimson Cyclone') { return 'Crimson Strike'; }
+      return 'Crimson Cyclone';
+    }
+    if (attunementSeconds > time) {
+      if (actionData.some((element) => element.name === 'Ruby Rite')) { return 'Ruby Rite'; }
+      if (actionData.some((element) => element.name === 'Ruby Ruin III')) { return 'Ruby Ruin III'; }
+      if (actionData.some((element) => element.name === 'Ruby Ruin II')) { return 'Ruby Ruin II'; }
+      return 'Ruby Ruin';
+    }
+  }
+
+  if (attunement === 'Titan') {
+    // Favor action is OGCD so not listed here
+    if (attunementSeconds > time) {
+      if (actionData.some((element) => element.name === 'Topaz Rite')) { return 'Topaz Rite'; }
+      if (actionData.some((element) => element.name === 'Topaz Ruin III')) { return 'Topaz Ruin III'; }
+      if (actionData.some((element) => element.name === 'Topaz Ruin II')) { return 'Topaz Ruin II'; }
+      return 'Topaz Ruin';
+    }
+  }
+
+
+  if (attunement === 'Garuda') {
+    calculateDelay({ })
+    if (checkStatusDuration({ name: 'Garuda\'s Favor', statusArray }) > 0) {
+      return 'Slipstream';
+    }
+    if (attunementSeconds > time) {
+      if (actionData.some((element) => element.name === 'Emerald Rite')) { return 'Emerald Rite'; }
+      if (actionData.some((element) => element.name === 'Emerald Ruin III')) { return 'Emerald Ruin III'; }
+      if (actionData.some((element) => element.name === 'Emerald Ruin II')) { return 'Emerald Ruin II'; }
+      return 'Emerald Ruin';
+    }
+  }
+
 
   // Ruin for filling gaps
   if (actionData.some((element) => element.name === 'Ruin III')) { return 'Ruin III'; }
